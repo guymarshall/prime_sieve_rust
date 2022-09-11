5745794ba815f1cf13b56308b80cdd5ead46f224
Guy Marshall <guymarshall9@gmail.com>
2022-09-11 21:00:22 +0100

    Add ceiling_root

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ fn main() {
     let user_input: i32 = user_input::get_user_input("Enter a positive integer: ");
     let numbers: Vec<i32> = math::number_to_vector(user_input);
 
-    println!("Numbers: {:?}", numbers);
+    let ceiling_root: i32 = (user_input as f64).sqrt().ceil() as i32;
+
+    println!("Square root: {}", ceiling_root);
 }
