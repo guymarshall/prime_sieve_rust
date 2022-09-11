1d90e31d09f16e013074ea6c1dd3daf5261434b7
Guy Marshall <guymarshall9@gmail.com>
2022-09-11 20:57:33 +0100

    Add user_input and numbers variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ mod math;
 mod user_input;
 
 fn main() {
-    println!("Hello, world!");
+    let user_input: i32 = user_input::get_user_input("Enter a positive integer: ");
+    let numbers: Vec<i32> = math::number_to_vector(user_input);
+
+    println!("Numbers: {:?}", numbers);
 }
