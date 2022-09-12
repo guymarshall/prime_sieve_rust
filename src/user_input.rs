dd23b2f734e1fe5c81b5f107cc840d7b88651633
Guy Marshall <guy@cslgroupservices.co.uk>
2022-09-12 12:17:35 +0100

    Remove unnecessary return for functions

--- src/user_input.rs
+++ src/user_input.rs
@@ -5,9 +5,11 @@ pub fn get_user_input(prompt: &str) -> i32 {
 
     let mut user_input: String = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
 
     let number: i32 = user_input.trim().parse().expect("Please enter an integer!");
 
-    return number;
+    number
 }
